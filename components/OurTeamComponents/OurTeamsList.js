72a84ea013010a060120a31df094ec893739aa0e
sibiraj-002 <shansibi2002@gmail.com>
2024-06-25 10:28:40 +0530

    Home page fixed, teams red-card issue fixed

--- components/OurTeamComponents/OurTeamsList.js
+++ components/OurTeamComponents/OurTeamsList.js
@@ -118,16 +118,18 @@ const OurTeamsList = () => {
                                     ))}
 
                                     {/* ADDITIONAL STATIC CARD FOR HIRING */}
-                                    <div className="col-lg-3 col-md-6 col-sm-12 d-flex flex-column p-3">
-                                        <div className="custom-height d-flex flex-column p-3 mt-0 rounded-4 justify-content-center align-items-center text-white iv-bg">
+                                    <div className="col-lg-3 col-md-6 col-sm-12 d-flex flex-column justify-content-between gap-2  p-3">
+                                        <div className="d-flex flex-column p-3 mt-0 rounded-4 justify-content-center align-items-center h-100 text-white iv-bg">
                                             <p className="fs-3 mb-0 text-center">
                                                 <a className="text-white text-decoration-none" target="_blank" href="/careers">
                                                     Want to claim this spot? We’re hiring
                                                 </a>
                                             </p>
                                         </div>
+                                        <div className='opacity-0'>
+                                            .
+                                        </div>
                                     </div>
-
                                 </div>
                             </>
                         )}
@@ -217,14 +219,17 @@ const OurTeamsList = () => {
                                         </div>
                                     ))}
 
-                                    {/* ADDITIONAL STATIC CARD FOR HIRING */}
-                                    <div className="col-lg-3 col-md-6 col-sm-12 d-flex flex-column p-3">
-                                        <div className="custom-height d-flex flex-column p-3 mt-0 rounded-4 justify-content-center align-items-center text-white iv-bg">
+                                     {/* ADDITIONAL STATIC CARD FOR HIRING */}
+                                    <div className="col-lg-3 col-md-6 col-sm-12 d-flex flex-column justify-content-between gap-2  p-3">
+                                        <div className="d-flex flex-column p-3 mt-0 rounded-4 justify-content-center align-items-center h-100 text-white iv-bg">
                                             <p className="fs-3 mb-0 text-center">
                                                 <a className="text-white text-decoration-none" target="_blank" href="/careers">
                                                     Want to claim this spot? We’re hiring
                                                 </a>
                                             </p>
+                                        </div>
+                                        <div className='opacity-0'>
+                                            .
                                         </div>
                                     </div>
                                 </div>
